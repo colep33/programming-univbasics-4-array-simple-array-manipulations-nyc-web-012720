e939e690dce3eb6cfb0c0da3d61e92b9e78d2997
Cole Arthur Parham <coleparham@gmail.com>
2019-12-26 22:28:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array,array1)
   array.concat(array1)
 end
 
-def using_insert(array)
-  array.insert
+def using_insert(array,element)
+  array.insert(element)[3]
 end
 
 def using_uniq(array)

170bfcab6cb0558e957e63c028b533caa6710751
Cole Arthur Parham <coleparham@gmail.com>
2019-12-26 22:12:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,15 +15,15 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-
+  array.shift
 end
 
 def shift_with_args(array)
-
+  array.shift(2)
 end
 
-def using_concat(array1,array2)
-
+def using_concat(array,arrays)
+  array = arrays.concat
 end
 
 def using_insert(array)

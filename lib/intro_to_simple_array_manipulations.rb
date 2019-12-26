47be8853d57480a6ae76936887a3dd334e20273a
Cole Arthur Parham <coleparham@gmail.com>
2019-12-26 22:36:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,8 +38,8 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete_at(array,integer)
-  array.delete(integer)
+def using_delete_at(array)
+  array.delete[2]
 end
 
 def using_delete(array,string)
